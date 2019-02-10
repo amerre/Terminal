cd879bdda0bdbeae64c66d8216b6c0b91e8bda6c
amerre <adr.merre@gmail.com>
2019-02-10 09:32:34 +0100

    :construction: Building store features

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -3,23 +3,32 @@
     <p v-for="lines in display" v-bind:key="lines">{{ lines }}</p>
     <span class="input">
       <span id="inputSign">></span>
-      <input type="text" v-model="input" @keydown.enter="submit" autofocus>
+      <input type="text" v-model="getInput" @keydown.enter="submit" autofocus>
     </span>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    getInput: {
+      get() {
+        return this.$store.state.input;
+      },
+      set(value) {
+        this.$store.commit("INPUT_COMMIT", value);
+      }
+    }
+  },
   data() {
     return {
-      input: "",
-      display: ["Hello Worlde"]
+      display: [""]
     };
   },
   methods: {
     submit() {
-      this.display.push(this.input);
-      this.input = "";
+      this.display.push(this.getInput);
+      this.getInput = "";
     }
   }
 };
